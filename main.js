da67d77d7f95719fe87cbf4bdeb49b3e5fa233ec
Natalja Issar <78623661+sgu6ena@users.noreply.github.com>
2021-04-13 18:03:12 +0300

    добавлена адаптивная верстка

--- main.js
+++ main.js
@@ -2,8 +2,26 @@ const swiper = new Swiper('.slider-swiper', {
     // Optional parameters
     //direction: 'gorizontal',
     loop: true,
-    slidesPerView: 6,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+        1900: {
+            slidesPerView: 6,
+        },
+        1600: {
+            slidesPerView: 5,
+        },
+        1300: {
+            slidesPerView: 4,
+        },
+        1100: {
+            slidesPerView: 3,
+        },
+        800: {
+            slidesPerView: 2,
+        },
 
+    },
     // Navigation arrows
     navigation: {
         nextEl: '.channel-button-next',
@@ -11,3 +29,69 @@ const swiper = new Swiper('.slider-swiper', {
     },
 
 });
+
+const swiper1 = new Swiper('.slider-swiper-large', {
+    // Optional parameters
+    //direction: 'gorizontal',
+    loop: true,
+    slidesPerView: 1,
+    breakpoints: {
+        1900: {
+            slidesPerView: 3,
+        },
+        1100: {
+            slidesPerView: 2,
+        },
+    },
+    spaceBetween: 20,
+    // Navigation arrows
+    navigation: {
+        nextEl: '.channel-button-next-1',
+        prevEl: '.channel-button-prev-1',
+    },
+
+});
+
+
+const swiper2 = new Swiper('.slider-swiper-2', {
+    // Optional parameters
+    //direction: 'gorizontal',
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    // Navigation arrows
+    navigation: {
+        nextEl: '.channel-button-next-2',
+        prevEl: '.channel-button-prev-2',
+    },
+    breakpoints: {
+        1900: {
+            slidesPerView: 6,
+        },
+        1600: {
+            slidesPerView: 5,
+        },
+        1300: {
+            slidesPerView: 4,
+        },
+        1100: {
+            slidesPerView: 3,
+        },
+        800: {
+            slidesPerView: 2,
+        },
+    },
+});
+
+
+const $searchButton = document.querySelector('.mobile-search');
+const $mobileSearch = document.querySelector('.input-group');
+$searchButton.addEventListener('click', () => {
+    $mobileSearch.classList.toggle('is-open');
+});
+
+if (document.documentElement.scrollWidth < 640) {
+    swiper.destroy();
+    swiper1.destroy();
+    swiper2.destroy();
+}
